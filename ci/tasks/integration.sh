0c0a14d8ea1de297e9343bbb014fc3d6db3b1495
Alex Shumilov <ashumilov@pivotal.io>
2016-09-19 14:25:42 -0400

    fix integration inputs

--- ci/tasks/integration.sh
+++ ci/tasks/integration.sh
@@ -1,39 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-# required
-inputDir=  outputDir=  inputManifest=  versionFile=  artifactId=  packaging=
-
-# optional
-hostname=$CF_MANIFEST_HOST # default to env variable from pipeline
+inputDir=
 
 while [ $# -gt 0 ]; do
   case $1 in
     -i | --input-dir )
       inputDir=$2
-      shift
-      ;;
-    -o | --output-dir )
-      outputDir=$2
-      shift
-      ;;
-    -v | --version-file )
-      versionFile=$2
-      shift
-      ;;
-    -f | --input-manifest )
-      inputManifest=$2
-      shift
-      ;;
-    -a | --artifactId )
-      artifactId=$2
-      shift
-      ;;
-    -p | --packaging )
-      packaging=$2
-      shift
-      ;;
-    -n | --hostname )
-      hostname=$2
       shift
       ;;
     * )
@@ -52,45 +24,7 @@ error_and_exit() {
 if [ ! -d "$inputDir" ]; then
   error_and_exit "missing input directory: $inputDir"
 fi
-if [ ! -d "$outputDir" ]; then
-  error_and_exit "missing output directory: $outputDir"
-fi
-if [ ! -f "$versionFile" ]; then
-  error_and_exit "missing version file: $versionFile"
-fi
-if [ ! -f "$inputManifest" ]; then
-  error_and_exit "missing input manifest: $inputManifest"
-fi
-if [ -z "$artifactId" ]; then
-  error_and_exit "missing artifactId!"
-fi
-if [ -z "$packaging" ]; then
-  error_and_exit "missing packaging!"
-fi
 
-# copy the jar file to the output directory
-version=`cat $versionFile`
-artifactName="${artifactId}-${version}.${packaging}"
+cd $inputDir
 
-inputArtifact="$inputDir/$artifactName"
-outputArtifact="$outputDir/$artifactName"
-
-if [ ! -f "$inputArtifact" ]; then
-  error_and_exit "can not find artifact: $inputArtifact"
-fi
-
-cp $inputArtifact $outputArtifact
-
-# copy the manifest to the output directory and process it
-outputManifest=$outputDir/manifest.yml
-
-cp $inputManifest $outputManifest
-
-# the path in the manifest is always relative to the manifest itself
-sed -i -- "s|path: .*$|path: $artifactName|g" $outputManifest
-
-if [ ! -z "$hostname" ]; then
-  sed -i "s|host: .*$|host: ${hostname}|g" $outputManifest
-fi
-
-cat $outputManifest
+./mvnw clean verify
