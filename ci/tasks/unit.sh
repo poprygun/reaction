3aaec83ffb7c23020f448865d633af8a7602f32f
Alex Shumilov <ashumilov@pivotal.io>
2016-09-21 13:47:30 -0400

    change out dir

--- ci/tasks/unit.sh
+++ ci/tasks/unit.sh
@@ -26,10 +26,16 @@ if [ ! -d "$inputDir" ]; then
   error_and_exit "missing input directory: $inputDir"
 fi
 
-cd $inputDir
-
 output_body_file=email-out/$output_body_file
 output_subject_file=email-out/$output_subject_file
+
+echo "here i am -----------------"
+pwd
+echo "here i am -----------------"
+ls -l $output_body_file
+echo "here i am -----------------"
+
+cd $inputDir
 
 ./mvnw clean test --log-file $output_body_file
 echo -e "This is the build unit test result" > $output_subject_file
