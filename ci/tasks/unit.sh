1d469490e67ee1b58f02d59ebe16edf345f39fcb
Alex Shumilov <ashumilov@pivotal.io>
2016-09-21 13:55:02 -0400

    change out dir

--- ci/tasks/unit.sh
+++ ci/tasks/unit.sh
@@ -29,13 +29,15 @@ fi
 output_body_file=email-out/$output_body_file
 output_subject_file=email-out/$output_subject_file
 
+echo -e "This is the build unit test result" > $output_subject_file
+
 echo "here i am -----------------"
 pwd
 echo "here i am -----------------"
-ls -l $output_body_file
+ls -l $output_subject_file
 echo "here i am -----------------"
 
 cd $inputDir
 
 ./mvnw clean test --log-file $output_body_file
-echo -e "This is the build unit test result" > $output_subject_file
+
